db25fe444c1f51d2c655ba66748368f5c1fc2e94
Ana Sofia Fonseca Lopes <anfonsec@c1r8s4.42lisboa.com>
2022-11-30 17:21:26 +0000

    segundo

--- ft_printf.c
+++ ft_printf.c
@@ -6,33 +6,51 @@
 /*   By: anfonsec <anfonsec@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 14:26:00 by anfonsec          #+#    #+#             */
-/*   Updated: 2022/11/30 15:37:31 by anfonsec         ###   ########.fr       */
+/*   Updated: 2022/11/30 17:15:36 by anfonsec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "libftprintf.h"
 
-static int	f_format(const char *str, int *i, va_list ap)
+static int	f_format(va_list args, const char *str, int *i)
 {
-	int	t;
+	int	print_lenght;
+
+	if (args == 'c')
+		print_lenght += ft_printchar (va_arg (args, int *i));
+	else if (args == 's')
+		print_lenght += ft_printstr (va_arg (args, str));
+	else if (args == 'p')
+		print_lenght += ft_printpointer (va_arg (args, unsigned long long);
+	else if (args == 'd')
+		print_lenght += ft_print_nbr((args, int));
+	else if (args == 'i')
+		print_lenght += ft_print_unsig((args, int));
+	else if (args == 'u')
+		print_lenght += ft_print_un_nbr((args, int));
+	else if (args == 'x'|| == 'X')
+		print_lenght += ft_print_nbr_hex((args, int));
+	else if (args == '%')
+		print_lenght += ft_print_perc((args, int));
 	
 }
+
 int	ft_printf(const char *str, ...)
 {
 	int		i;
 	int		j;
-	va_list	ap;
+	va_list	args;
 
-	va_start(ap, str);
+	va_start(args, str);
 	i = 0;
 	j = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] == '%')
 		{
-			if (ft_strchr("cspdiuxX%"), str[++i])
+			if (ft_strchr("cspdiuxX%", str[++i]))
 			{
-				j += f_format(str, &i, ap);
+				j += f_format(str, &i, args);
 				i++;
 				continue ;
 			}
@@ -41,5 +59,24 @@ int	ft_printf(const char *str, ...)
 		j++;
 		i++;
 	}
-	va_end(ap);
+	va_end(args);
+	return (j);
+	
 }
+
+/*char	*ft_strchr(const char *s, int c)
+
+{
+	char	*str;
+
+	str = (char *)s;
+	while (*str != (unsigned char)c)
+	{
+		if (*str == '\0')
+		{
+			return (NULL);
+		}
+		str++;
+	}
+	return (str);
+}*/
